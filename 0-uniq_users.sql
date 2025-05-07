6674d082a12e38bc873f102956cc658b0586ed05
Mareeam89 <mareeamraj@gmail.com>
2025-05-07 11:01:06 +0100

    Clean up SQL files and add config/license

--- 0-uniq_users.sql
+++
@@ -1,9 +0,0 @@
--- Script to create `users` table with unique email and auto-incrementing id
--- If table already exists, do not fail
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
-
